740b3bb3b0409e2f5baeecdeb2ba2e09059f7881
xangma <xangma@gmail.com>
2017-08-01 08:49:19 +0100

    latest

--- plots.py
+++ plots.py
@@ -11,6 +11,11 @@ from matplotlib.colors import LogNorm
 import os
 import logging
 import numpy.ma as ma
+import itertools as it
+from sklearn import tree
+from sklearn import metrics
+from sklearn import covariance
+from collections import OrderedDict
 
 plots_log=logging.getLogger('plots')
 
@@ -168,28 +173,29 @@ def plot_feat(feat_importance,feat_names,n_run):
 
 def plot_feat_per_class(one_vs_all_results,feat_names,n):
     outname=[]
-    plt.figure()
-    for i in range(len(one_vs_all_results)):
-        plt.step(numpy.array(range(len(one_vs_all_results[i]['feat_importance'])+2)),numpy.concatenate(([0],one_vs_all_results[i]['feat_importance'],[0])),label='class == %s' %one_vs_all_results[i]['uniquetarget_tr_loop'][0][0])
-#        plt.scatter(numpy.array(range(len(one_vs_all_results[i]['feat_importance'])))+1,one_vs_all_results[i]['feat_importance'],s=2)
-#        plt.plot(numpy.array(range(len(one_vs_all_results[i]['feat_importance'])))+1,one_vs_all_results[i]['feat_importance'],label='class == %s' %one_vs_all_results[i]['class_ID'])
-    plt.axvspan(5.0, 15.0, color='red', alpha=0.3)
-    plt.axvspan(20.0, 30.0, color='red', alpha=0.3)
-    plt.axvspan(35.0, 45.0, color='red', alpha=0.3)
-    plt.legend()
-    plt.title('Feature importance per class')
-    plt.xlabel('Features')
-    plt.ylabel('Feat_Importance')
-    plt.xlim(0.0,len(feat_names))
-    plt.ylim(0,0.25)
-    plt.xticks(numpy.array(range(len(one_vs_all_results[0]['feat_importance'])))+0.5,feat_names, size=8,rotation='vertical')
-#    plt.xticks(numpy.array(range(len(feat_arr[0]))))
-#    plt.minorticks_on()
-    plt.grid(alpha=0.4,which='both')
-    plt.tight_layout()
-    outname = 'plots/Feature_imp_per_class_%s.png' %n
-    plt.savefig(outname)
-    plt.close()
+    if settings.one_vs_all == 1:
+        plt.figure()
+        for i in range(len(one_vs_all_results)):
+            plt.step(numpy.array(range(len(one_vs_all_results[i]['feat_importance'])+2)),numpy.concatenate(([0],one_vs_all_results[i]['feat_importance'],[0])),label='class == %s' %one_vs_all_results[i]['uniquetarget_tr'][0][0])
+    #        plt.scatter(numpy.array(range(len(one_vs_all_results[i]['feat_importance'])))+1,one_vs_all_results[i]['feat_importance'],s=2)
+    #        plt.plot(numpy.array(range(len(one_vs_all_results[i]['feat_importance'])))+1,one_vs_all_results[i]['feat_importance'],label='class == %s' %one_vs_all_results[i]['class_ID'])
+        plt.axvspan(5.0, 15.0, color='red', alpha=0.3)
+        plt.axvspan(20.0, 30.0, color='red', alpha=0.3)
+        plt.axvspan(35.0, 45.0, color='red', alpha=0.3)
+        plt.legend()
+        plt.title('Feature importance per class')
+        plt.xlabel('Features')
+        plt.ylabel('Feat_Importance')
+        plt.xlim(0.0,len(feat_names))
+        plt.ylim(0,0.25)
+        plt.xticks(numpy.array(range(len(one_vs_all_results[0]['feat_importance'])))+0.5,feat_names, size=8,rotation='vertical')
+    #    plt.xticks(numpy.array(range(len(feat_arr[0]))))
+    #    plt.minorticks_on()
+        plt.grid(alpha=0.4,which='both')
+        plt.tight_layout()
+        outname = 'plots/Feature_imp_per_class_%s.png' %n
+        plt.savefig(outname)
+        plt.close()
     return outname
 
 def plot_feat_per_class_oth(one_vs_all_results,n_filt,n_colours):
@@ -321,7 +327,7 @@ def plot_col_cont(XXpredict,result,yypredict,feat_names,filtstats,uniquetarget_t
                 #
                 outliermask1=is_outlier(XXpredict[:,i])
                 outliermask2 = is_outlier(cont[l][i])
-                totalmask_tpfp = (T_p_F_p) & (~outliermask2) & (~outliermask1)
+                totalmask_tpfp = (T_p_F_p)& (~outliermask2) & (~outliermask1)
                 totalmask_tpfn = (T_p_F_n) & (~outliermask2) & (~outliermask1)
                 totalmask_tp = (T_p) & (~outliermask2) & (~outliermask1)
                 totalmask_true = (true_class) & (~outliermask2) & (~outliermask1)
@@ -547,8 +553,280 @@ def plot_pearson(feat_names):
             plt.close(fig)
             outnamelist.append(outname)
     return outnamelist
+
+def decision_boundaries_MINT(XX,XXpredict,yy,MINT_feats,MINT_feat_names,uniquetarget_tr):
+    outnamelist=[]
+    dirs=os.listdir(path)
+    savedir='db_MINT' # Check if directory exists, if not, create
+    fullsavedir=path+savedir+'/'
+    if savedir not in dirs:
+        os.mkdir(fullsavedir)
+    if settings.plot_decision_boundaries_MINT == 1:
+        plot_step = 0.1  # fine step width for decision surface contours
+        plot_step_coarser = 0.5  # step widths for coarse classifier guesses
+        if settings.make_binary == 0:
+            plot_colors = "ryb"
+            cmap = plt.cm.RdYlBu
+        else:
+            plot_colors="rb"
+            cmap = plt.cm.RdBu
+        n_classes = len(uniquetarget_tr[0])
+        n_estimators = 256
         
+        MLA = get_function(settings.MLA)        
+        clf = MLA().set_params(**settings.MLAset)
         
+        combs_MINT_index = list(it.combinations(MINT_feats['best_feats'],2))
+        combs = list(it.combinations(range(len(MINT_feats['best_feats'])),2))
+        
+        for i in range(len(combs_MINT_index)):
+            fig=plt.figure()
+            x_min, x_max = XX[:, combs_MINT_index[i][0]].min() - 1, XX[:, combs_MINT_index[i][0]].max() + 1
+            y_min, y_max = XX[:, combs_MINT_index[i][1]].min() - 1, XX[:, combs_MINT_index[i][1]].max() + 1
+            xxx, yyy = numpy.meshgrid(numpy.arange(x_min, x_max, plot_step),numpy.arange(y_min, y_max, plot_step))
+            
+            clf = clf.fit(numpy.transpose(numpy.vstack((XX[:,combs_MINT_index[i][0]],XX[:,combs_MINT_index[i][1]]))),yy)        
+            
+            estimator_alpha = 1.0 / len(clf.estimators_)
+            for tree in clf.estimators_:
+                Z = tree.predict(numpy.c_[xxx.ravel(), yyy.ravel()])
+                Z = Z.reshape(xxx.shape)
+                cs = plt.contourf(xxx, yyy, Z, alpha=estimator_alpha, cmap=cmap)  
+            xx_coarser, yy_coarser = numpy.meshgrid(numpy.arange(x_min, x_max, plot_step_coarser),numpy.arange(y_min, y_max, plot_step_coarser))
+            Z_points_coarser = clf.predict(numpy.c_[xx_coarser.ravel(), yy_coarser.ravel()]).reshape(xx_coarser.shape)
+            cs_points = plt.scatter(xx_coarser, yy_coarser, s=15, c=Z_points_coarser, cmap=cmap, edgecolors="none")
+            for j, c in zip(range(n_classes), plot_colors):
+                idx = numpy.where(yy == j)
+                plt.scatter(XX[idx, combs_MINT_index[i][0]], XX[idx, combs_MINT_index[i][1]], c=c, label=uniquetarget_tr[0][j], cmap=cmap)
+            plt.legend()
+            plt.xlabel(MINT_feat_names[combs[i][0]])
+            plt.ylabel(MINT_feat_names[combs[i][1]])
+            outname=fullsavedir+'db'+str(i)+'.png'
+            plt.savefig(outname)
+            plt.close(fig)
+            outnamelist.append(outname)
+    return outnamelist
+
+def decision_boundaries(XX,XXpredict,yy,yypredict,feat_names,uniquetarget_tr):
+    outnamelist=[]
+    dirs=os.listdir(path)
+    savedir='db' # Check if directory exists, if not, create
+    fullsavedir=path+savedir+'/'
+    if savedir not in dirs:
+        os.mkdir(fullsavedir)
+    if settings.plot_decision_boundaries == 1:
+        plot_step = 0.1  # fine step width for decision surface contours
+        plot_step_coarser = 0.5  # step widths for coarse classifier guesses
+        if settings.make_binary == 0:
+            plot_colors = "ryb"
+            cmap = plt.cm.RdYlBu
+        else:
+            plot_colors="rb"
+            cmap = plt.cm.RdBu
+        n_classes = len(uniquetarget_tr[0])
+        n_estimators = 256
+        
+        MLA = get_function(settings.MLA)        
+        clf = MLA().set_params(**settings.MLAset)
+        
+    #        combs_MINT_index = list(it.combinations(MINT_feats['best_feats'],2))
+        combs = list(it.combinations(range(len(feat_names)),2))
+        
+        for i in range(len(combs)):
+            fig=plt.figure()
+            x_min, x_max = XX[:, combs[i][0]].min() - 1, XX[:, combs[i][0]].max() + 1
+            y_min, y_max = XX[:, combs[i][1]].min() - 1, XX[:, combs[i][1]].max() + 1
+            xxx, yyy = numpy.meshgrid(numpy.arange(x_min, x_max, plot_step),numpy.arange(y_min, y_max, plot_step))
+            
+            clf = clf.fit(numpy.transpose(numpy.vstack((XX[:,combs[i][0]],XX[:,combs[i][1]]))),yy)        
+            result= clf.predict(numpy.transpose(numpy.vstack((XXpredict[:,combs[i][0]],XXpredict[:,combs[i][1]]))))
+            accuracy = metrics.accuracy_score(result,yypredict)
+            estimator_alpha = 1.0 / len(clf.estimators_)
+            for tree in clf.estimators_:
+                Z = tree.predict(numpy.c_[xxx.ravel(), yyy.ravel()])
+                Z = Z.reshape(xxx.shape)
+                cs = plt.contourf(xxx, yyy, Z, alpha=estimator_alpha, cmap=cmap)  
+            xx_coarser, yy_coarser = numpy.meshgrid(numpy.arange(x_min, x_max, plot_step_coarser),numpy.arange(y_min, y_max, plot_step_coarser))
+            Z_points_coarser = clf.predict(numpy.c_[xx_coarser.ravel(), yy_coarser.ravel()]).reshape(xx_coarser.shape)
+            cs_points = plt.scatter(xx_coarser, yy_coarser, s=15, c=Z_points_coarser, cmap=cmap, edgecolors="none")
+            for j, c in zip(range(n_classes), plot_colors):
+                idx = numpy.where(yy == j)
+                plt.scatter(XX[idx, combs[i][0]], XX[idx, combs[i][1]], c=c, label=uniquetarget_tr[0][j], cmap=cmap)
+            plt.legend()
+            axes = plt.gca()
+            yaxes = axes.get_ylim()
+            absy=yaxes[1]-yaxes[0]
+            deltay=0.145-yaxes[0]
+            xaxes = axes.get_xlim()
+            absx=xaxes[1]-xaxes[0]
+            deltax=0.145-xaxes[0]
+            plt.axhline(y=.145,xmax=deltax/absx, linewidth=2,color='black')
+            plt.axvline(x=.145,ymax=deltay/absy, linewidth=2,color='black')
+            plt.xlabel(feat_names[combs[i][0]])
+            plt.ylabel(feat_names[combs[i][1]])
+            plt.title('Accuracy: %s' %accuracy)
+            outname=fullsavedir+'db'+str(i)+'.png'
+            plt.savefig(outname)
+            plt.close(fig)
+            outnamelist.append(outname)
+    return outnamelist
+
+def decision_boundaries_DT(XX,XXpredict,yy,yypredict,feat_names,uniquetarget_tr):
+    outnamelist=[]
+    dirs=os.listdir(path)
+    savedir='db' # Check if directory exists, if not, create
+    fullsavedir=path+savedir+'/'
+    if savedir not in dirs:
+        os.mkdir(fullsavedir)
+    if settings.plot_decision_boundaries_DT == 1:
+        plot_step = 0.1  # fine step width for decision surface contours
+        plot_step_coarser = 0.5  # step widths for coarse classifier guesses
+        if settings.make_binary == 0:
+            plot_colors = "ryb"
+            cmap = plt.cm.RdYlBu
+        else:
+            plot_colors="rb"
+            cmap = plt.cm.RdBu
+        n_classes = len(uniquetarget_tr[0])
+        n_estimators = 256
+        MLA = 'sklearn.tree.DecisionTreeClassifier'                             # Which MLA to load
+        MLAset = {'max_depth':10} 
+        MLA = get_function(MLA)        
+        clf = MLA().set_params(**MLAset)
+        
+    #        combs_MINT_index = list(it.combinations(MINT_feats['best_feats'],2))
+        combs = list(it.combinations(range(len(feat_names)),2))
+        
+        for i in range(len(combs)):
+            fig=plt.figure()
+            x_min, x_max = XX[:, combs[i][0]].min() - 1, XX[:, combs[i][0]].max() + 1
+            y_min, y_max = XX[:, combs[i][1]].min() - 1, XX[:, combs[i][1]].max() + 1
+            xxx, yyy = numpy.meshgrid(numpy.arange(x_min, x_max, plot_step),numpy.arange(y_min, y_max, plot_step))
+            
+            clf = clf.fit(XX[:,combs[i][0],None],yy)        
+            result= clf.predict(XXpredict[:,combs[i][0],None])
+            accuracy = metrics.accuracy_score(result,yypredict)
+#            estimator_alpha = 1.0 / len(clf.estimators_)
+#            for tree in clf.estimators_:
+            Z = clf.predict(numpy.c_[xxx.ravel()])
+            Z = Z.reshape(xxx.shape)
+            cs = plt.contourf(xxx, yyy, Z, cmap=cmap)  
+            xx_coarser, yy_coarser = numpy.meshgrid(numpy.arange(x_min, x_max, plot_step_coarser),numpy.arange(y_min, y_max, plot_step_coarser))
+            Z_points_coarser = clf.predict(numpy.c_[xx_coarser.ravel()]).reshape(xx_coarser.shape)
+            cs_points = plt.scatter(xx_coarser, yy_coarser, s=15, c=Z_points_coarser, cmap=cmap, edgecolors="none")
+            for j, c in zip(range(n_classes), plot_colors):
+                idx = numpy.where(yy == j)
+                plt.scatter(XX[idx, combs[i][0]], XX[idx, combs[i][1]], c=c, label=uniquetarget_tr[0][j], cmap=cmap)
+            plt.legend()
+            axes = plt.gca()
+            yaxes = axes.get_ylim()
+            absy=yaxes[1]-yaxes[0]
+            deltay=0.145-yaxes[0]
+            xaxes = axes.get_xlim()
+            absx=xaxes[1]-xaxes[0]
+            deltax=0.145-xaxes[0]
+            plt.axhline(y=.145,xmax=deltax/absx, linewidth=2,color='black')
+            plt.axvline(x=.145,ymax=deltay/absy, linewidth=2,color='black')
+            plt.xlabel(feat_names[combs[i][0]])
+            plt.ylabel(feat_names[combs[i][1]])
+            plt.title('Decision Tree Accuracy: %s' %accuracy)
+            outname=fullsavedir+'db'+str(i)+'.png'
+            plt.savefig(outname)
+            plt.close(fig)
+            outnamelist.append(outname)
+    return outnamelist
+
+def plot_depth_acc(XXpredict,result,yypredict,feat_names,filtstats,uniquetarget_tr,dered_tr_r,dered_pr_r):
+    outnamelist=[]
+    if settings.plot_depth_acc==1:
+        dirs=os.listdir(path)
+        savedir='plot_depth_acc' # Check if directory exists, if not, create
+        fullsavedir=path+savedir+'/'
+        if savedir not in dirs:
+            os.mkdir(fullsavedir)       
+#        for i in range(len(feat_names)): # Plot for all filters
+#                fig=plt.figure()
+#            f, (axarr) = plt.subplots(2, 2)#,figsize=(14, 5))
+            
+        true_class = yypredict==result
+        outliermask1=is_outlier(dered_pr_r)
+        totalmask_true = (true_class) & (~outliermask1)
+        # plot
+        hist_true,bin_edges,patches = plt.hist(dered_pr_r[totalmask_true], bins=80)
+        hist_total,bin_edges_tot,patches_tot= plt.hist(dered_pr_r[~outliermask1], bins=80,range=(bin_edges.min(),bin_edges.max()))
+        hist_acc = (hist_true/hist_total)
+        hist_acc = numpy.nan_to_num(hist_acc)
+        hist_acc = hist_acc*100
+        plt.figure()
+        plt.step(bin_edges[:-1],hist_acc)
+        plt.ylim(0,110)
+        plt.title('Depth Accuracy in DERED_R')
+        plt.xlabel('DERED_R')
+        plt.ylabel('Accuracy (%)')
+        outname='plots/'+savedir+'/depth_acc_DR_R.png'
+        outnamelist.append(outname)
+        plt.tight_layout()
+        plt.savefig(outname)
+        plt.close()
+    return outnamelist
+
+def plot_oob_err_rate(XX,yy):
+    outnamelist=[]
+    if settings.plot_oob_err_rate==1:
+        dirs=os.listdir(path)
+        savedir='plot_oob_err_rate' # Check if directory exists, if not, create
+        fullsavedir=path+savedir+'/'
+        if savedir not in dirs:
+            os.mkdir(fullsavedir)    
+        MLA = get_function(settings.MLA)        
+        clf = MLA().set_params(**settings.MLAset)
+        ensemble_clfs = [
+            ("RandomForestClassifier, max_features='sqrt'",
+                MLA(warm_start=True, oob_score=True,
+                                       max_features="sqrt")),
+            ("RandomForestClassifier, max_features='log2'",
+                MLA(warm_start=True, max_features='log2',
+                                       oob_score=True)),
+            ("RandomForestClassifier, max_features=None",
+                MLA(warm_start=True, max_features=None,
+                                       oob_score=True))]
+        # Map a classifier name to a list of (<n_estimators>, <error rate>) pairs.
+        error_rate = OrderedDict((label, []) for label, _ in ensemble_clfs)
+        
+        # Range of `n_estimators` values to explore.
+        min_estimators = 15
+        max_estimators = 175
+        
+        for label, clf in ensemble_clfs:
+            for i in range(min_estimators, max_estimators + 1):
+                clf.set_params(n_estimators=i)
+                clf.fit(XX, yy)
+                # Record the OOB error for each `n_estimators=i` setting.
+                oob_error = 1 - clf.oob_score_
+                error_rate[label].append((i, oob_error))
+        # Generate the "OOB error rate" vs. "n_estimators" plot.
+        for label, clf_err in error_rate.items():
+            xs, ys = zip(*clf_err)
+            plt.plot(xs, ys, label=label)
+        plt.xlim(min_estimators, max_estimators)
+        plt.xlabel("n_estimators")
+        plt.ylabel("OOB error rate")
+        plt.legend(loc="upper right")
+#        plt.show()
+        outname='plots/'+savedir+'/oob_err_rate.png'
+        outnamelist.append(outname)
+        plt.tight_layout()
+        plt.savefig(outname)
+        plt.close()
+    return outnamelist
+
+def get_function(function_string):
+    import importlib
+    module, function = function_string.rsplit('.', 1)
+    module = importlib.import_module(module)
+    function = getattr(module, function)
+    return function
+
 def is_outlier(points, thresh=6):
     """
     Returns a boolean array with True if points are outliers and False 
